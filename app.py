be012c2b78dce7cd9aa3e559e260ce594ed7fba5
ademsim <43340117+ademsim@users.noreply.github.com>
2026-08-20 06:23:55 +0300

    Update app.py

--- app.py
+++ app.py
@@ -6,16 +6,16 @@ import gdown
 import keras
 
 FILE_ID = 'https://drive.google.com/file/d/1xbNnagCXfdm0CnYzYWAh3nJLmkOFQkDl/view?usp=sharing'
-MODEL_PATH = 'skin_cancer_model.h5' # veya .keras
+MODEL_PATH = 'skin_cancer_model.keras'
 
 @st.cache_resource
 def load_my_model():
     if not os.path.exists(MODEL_PATH):
-        with st.spinner("Model buluttan indiriliyor, lütfen bekleyin..."):
+        with st.spinner("Model downloading, please wait..."):
             url = f'https://drive.google.com/uc?id={FILE_ID}'
             gdown.download(url, MODEL_PATH, quiet=False, fuzzy=True)
             
-    # Modern Keras 3 ile yükleme
+    # Keras-loading function
     return keras.models.load_model(MODEL_PATH)
 
 model = load_my_model()
