8b54aa9c8425abe966aa4fe5259081729afbb9e7
ademsim <43340117+ademsim@users.noreply.github.com>
2026-08-20 06:33:35 +0300

    Update app.py

--- app.py
+++ app.py
@@ -5,16 +5,16 @@ import streamlit as st
 import gdown
 import keras
 
-# Google Drive 
-FILE_ID = 'https://drive.google.com/file/d/1xbNnagCXfdm0CnYzYWAh3nJLmkOFQkDl/view?usp=drive_link'
+FILE_ID = '1xbNnagCXfdm0CnYzYWAh3nJLmkOFQkDl'
 MODEL_PATH = 'skin_cancer_model.keras' 
 
 @st.cache_resource
 def load_my_model():
     if not os.path.exists(MODEL_PATH):
         with st.spinner("Model downloading, please wait..."):
-            url = f'https://drive.google.com/uc?id={FILE_ID}&confirm=t'
-            gdown.download(url, MODEL_PATH, quiet=False)
+            # bypassing large file in G-Drive
+            url = f'https://drive.google.com/uc?export=download&id={FILE_ID}'
+            gdown.download(url, MODEL_PATH, quiet=False, fuzzy=True)
             
     return keras.models.load_model(MODEL_PATH)
 
