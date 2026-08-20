f18c5d841d4e216f14fca21fe2843805573adef6
ademsim <43340117+ademsim@users.noreply.github.com>
2026-08-20 06:22:31 +0300

    Update app.py

--- app.py
+++ app.py
@@ -1,19 +1,21 @@
+import os
+os.environ["KERAS_BACKEND"] = "torch"
+
 import streamlit as st
 import gdown
-import os
-import keras 
+import keras
 
-FILE_ID = 'https://drive.google.com/file/d/11jAyAWJjgFHejigcrxwgZDSxjjUrizph/view?usp=drive_link'
-MODEL_PATH = 'skin_cancer_TL.h5'
+FILE_ID = 'https://drive.google.com/file/d/1xbNnagCXfdm0CnYzYWAh3nJLmkOFQkDl/view?usp=sharing'
+MODEL_PATH = 'skin_cancer_model.h5' # veya .keras
 
 @st.cache_resource
 def load_my_model():
     if not os.path.exists(MODEL_PATH):
-        with st.spinner("Model downloading, please wait..."):
+        with st.spinner("Model buluttan indiriliyor, lütfen bekleyin..."):
             url = f'https://drive.google.com/uc?id={FILE_ID}'
-            gdown.download(url, MODEL_PATH, quiet=False)
-    
-    # Keras-loading function
+            gdown.download(url, MODEL_PATH, quiet=False, fuzzy=True)
+            
+    # Modern Keras 3 ile yükleme
     return keras.models.load_model(MODEL_PATH)
 
 model = load_my_model()
