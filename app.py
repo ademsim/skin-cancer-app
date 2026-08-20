62ef149fb82b8c248cc29e417a5b38ce829bc791
ademsim <43340117+ademsim@users.noreply.github.com>
2026-08-20 06:42:06 +0300

    Update app.py

--- app.py
+++ app.py
@@ -2,19 +2,23 @@ import os
 os.environ["KERAS_BACKEND"] = "torch"
 
 import streamlit as st
-import urllib.request
+import gdown
 import keras
 
 FILE_ID = '1xbNnagCXfdm0CnYzYWAh3nJLmkOFQkDl'
-MODEL_PATH = 'skin_cancer_model.keras' 
+MODEL_PATH = 'skin_cancer_model.keras'
 
 @st.cache_resource
 def load_my_model():
-    if not os.path.exists(MODEL_PATH):
-        with st.spinner("Model downloading, please wait..."):
-            # bypass G-Drive for large file
-            url = f'https://drive.google.com/uc?export=download&id={FILE_ID}&confirm=t'
-            urllib.request.urlretrieve(url, MODEL_PATH)
+    if not os.path.exists(MODEL_PATH) or os.path.getsize(MODEL_PATH) < 1000:
+        # remove if not exist
+        if os.path.exists(MODEL_PATH):
+            os.remove(MODEL_PATH)
+            
+        with st.spinner("Model downlading, please wait..."):
+            url = f'https://drive.google.com/uc?id={FILE_ID}'
+            # gdown process
+            gdown.download(url, MODEL_PATH, quiet=False, thetar=False)
             
     return keras.models.load_model(MODEL_PATH)
 
