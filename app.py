386ab1af8e80d7cf09cbab24817539152849bed4
ademsim <43340117+ademsim@users.noreply.github.com>
2026-08-20 06:47:31 +0300

    Update app.py

--- app.py
+++ app.py
@@ -11,14 +11,13 @@ MODEL_PATH = 'skin_cancer_model.keras'
 @st.cache_resource
 def load_my_model():
     if not os.path.exists(MODEL_PATH) or os.path.getsize(MODEL_PATH) < 1000:
-        # remove if not exist
         if os.path.exists(MODEL_PATH):
             os.remove(MODEL_PATH)
             
-        with st.spinner("Model downlading, please wait..."):
+        with st.spinner("Model buluttan indiriliyor, lütfen bekleyin..."):
             url = f'https://drive.google.com/uc?id={FILE_ID}'
-            # gdown process
-            gdown.download(url, MODEL_PATH, quiet=False, thetar=False)
+            # Hiçbir ek parametre vermeden en sade ve kararlı çağrı
+            gdown.download(url, MODEL_PATH, quiet=False)
             
     return keras.models.load_model(MODEL_PATH)
 
